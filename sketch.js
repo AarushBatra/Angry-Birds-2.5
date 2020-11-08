3448d7f981a5f6312ddbe8dbc1b48cca1e897f11
AarushBatra <batraaarush1404@gmail.com>
2020-11-08 13:42:49 +0530

    Platform 1 uploaded by Aarush

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+platform1=new Ground(200 ,200,50,50);
 }
 
 function draw(){
@@ -61,4 +61,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    platform1.display();
 }
